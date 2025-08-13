606608b35e34d32888d554b4829db8d724204f3b
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-08-12 20:26:59 -0700

    fix: Forgot to export assets schema

--- schemaTypes/splash-n-shine/index.ts
+++ schemaTypes/splash-n-shine/index.ts
@@ -3,8 +3,11 @@ import { projectType } from './projectType'
 import { whyPointType } from './whyPointType'
 import { serviceType } from './serviceType'
 import { locationType } from './locationType'
+import { asset } from './assets'
+
 
 export const snsSchemaTypes = [
+  asset,
   postType,
   projectType,
   whyPointType,
